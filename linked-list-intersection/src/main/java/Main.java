ba059c48c32a66844c255778f00c07afc394dc48
anasabbal <anas.abbal66@gmail.com>
2023-07-30 15:01:31 +0100

    add more challenge

---
+++ linked-list-intersection/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        int intersection = sll.intersection();
+        System.out.println("\nIntersection node has the value (-1 means no intersection): " + intersection);
+    }
+}
